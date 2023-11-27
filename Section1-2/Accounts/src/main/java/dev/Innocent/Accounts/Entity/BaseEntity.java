f360832c228eaaeb992bc1090142ba557fcb9691
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-27 22:09:50 +0100

    BaseEnitity

---
+++ Section1-2/Accounts/src/main/java/dev/Innocent/Accounts/Entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package dev.Innocent.Accounts.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+}
