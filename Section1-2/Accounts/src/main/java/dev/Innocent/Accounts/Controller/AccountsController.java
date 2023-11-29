bc57f2971693b5f212fdeb4a58e514844c2604f4
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-29 16:00:28 +0100

    Feat | API Documentation

--- Section1-2/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
+++ Section1-2/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
@@ -4,6 +4,10 @@ import dev.Innocent.Accounts.Constants.AccountsConstants;
 import dev.Innocent.Accounts.DTO.CustomerDTO;
 import dev.Innocent.Accounts.DTO.Response.ResponseDTO;
 import dev.Innocent.Accounts.Service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in InnocentUdo's Bank",
+        description = "CRUD REST APIs in InnocentUdo's Bank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -20,6 +28,15 @@ import org.springframework.web.bind.annotation.*;
 public class AccountsController {
 
     private IAccountsService iAccountsService;
+
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer & Account inside InnocentUdo's Bank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO){
         iAccountsService.createAccount(customerDTO);
@@ -27,6 +44,14 @@ public class AccountsController {
                 .body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account  Details REST API",
+            description = "REST API to fetch Customer & Account details based on mobile number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp = "($|[0-9]{10})", message = "Mobile number must be 10 digits")
@@ -35,6 +60,20 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDTO);
     }
 
+    @Operation(
+            summary = "Update Account details REST API",
+            description = "REST API to  update Customer  & Account based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO>updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO){
         boolean isUpdated = iAccountsService.updateAccount(customerDTO);
@@ -48,6 +87,20 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account Details REST API",
+            description = "REST API to  delete Customer  & Account Details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam
                                                                 @Pattern(regexp = "($|[0-9]{10})", message = "Mobile number must be 10 digits")
