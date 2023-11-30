c639116254d68302f7c5a8ca4f2f0f15be802103
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-30 17:19:15 +0100

    Card Microservice

---
+++ Section1-2/Card/src/main/java/dev/Innocent/Card/CardApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardApplication.class, args);
+	}
+
+}
