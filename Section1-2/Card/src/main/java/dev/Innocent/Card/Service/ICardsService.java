d6ea666a15353094b233ddf66123e94f3c8b7644
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-30 18:03:54 +0100

    Card Microservice

---
+++ Section1-2/Card/src/main/java/dev/Innocent/Card/Service/ICardsService.java
@@ -0,0 +1,30 @@
+package dev.Innocent.Card.Service;
+
+import dev.Innocent.Card.DTO.CardsDto;
+
+public interface ICardsService {
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    void createCard(String mobileNumber);
+
+    /**
+     * @param mobileNumber - Input mobile Number
+     *  @return Card Details based on a given mobileNumber
+     */
+    CardsDto fetchCard(String mobileNumber);
+
+    /**
+     * @param cardsDto - CardsDto Object
+     * @return boolean indicating if the update of card details is successful or not
+     */
+    boolean updateCard(CardsDto cardsDto);
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of card details is successful or not
+     */
+    boolean deleteCard(String mobileNumber);
+
+
+}
