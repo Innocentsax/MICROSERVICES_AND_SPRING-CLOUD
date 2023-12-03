8a6289755778906964d5684462519a49c75febf9
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-03 08:37:06 +0100

    Deployment and Scalability

---
+++ Section5-6/v1-springboot/Accounts/src/main/java/dev/Innocent/Accounts/Mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package dev.Innocent.Accounts.Mapper;
+
+import dev.Innocent.Accounts.DTO.CustomerDTO;
+import dev.Innocent.Accounts.Entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDTO mapToCustomerDTO(Customer customer, CustomerDTO customerDTO){
+        customerDTO.setName(customer.getName());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setMobileNumber(customer.getMobileNumber());
+        return customerDTO;
+    }
+
+    public static Customer mapToCustomer(CustomerDTO customerDTO, Customer customer){
+        customer.setName(customerDTO.getName());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setMobileNumber(customerDTO.getMobileNumber());
+        return customer;
+    }
+}
---
+++ Section5-6/v1-springboot/Accounts/src/main/java/dev/Innocent/Accounts/Repository/AccountsRepository.java
@@ -0,0 +1,18 @@
+package dev.Innocent.Accounts.Repository;
+
+import dev.Innocent.Accounts.Entity.Accounts;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
+}
