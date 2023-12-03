bc2226a958e4f559ce67ea41a068c5d15447cbbd
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-03 15:07:06 +0100

    feat | Configuration Management

--- Section5-6/v1-springboot/Accounts/src/main/java/dev/Innocent/Accounts/DTO/AccountsContactInfoDTO.java
+++ Section5-6/v1-springboot/Accounts/src/main/java/dev/Innocent/Accounts/DTO/AccountsContactInfoDTO.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "account")
+@ConfigurationProperties(prefix = "accounts")
 public record AccountsContactInfoDTO(String message, Map<String, String>
         contactDetails, List<String> onCallSupport) {
 }
