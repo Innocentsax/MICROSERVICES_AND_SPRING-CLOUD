3f2cc499a45ee275292e83dfb77298de66b1c03c
Innocent Udo <innocentcharlesudo@gmail.com>
2023-12-04 08:47:38 +0100

    Spring Cloud Configuration

---
+++ Section5-6/v2-spring-cloud-config/configserver/src/main/java/dev/Innocent/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
---
+++ Section5-6/v2-spring-cloud-config/loan/src/test/java/dev/Innocent/Loan/loanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class loanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
