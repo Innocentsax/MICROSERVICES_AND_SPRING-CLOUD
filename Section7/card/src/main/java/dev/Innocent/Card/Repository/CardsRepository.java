d1db895d46e88da732c0516cb61f03b40326e5c2
Innocent Udo <innocentcharlesudo@gmail.com>
2023-12-06 11:42:38 +0100

    Add DB

---
+++ Section7/card/src/main/java/dev/Innocent/Card/Repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package dev.Innocent.Card.Repository;
+
+import dev.Innocent.Card.Entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
