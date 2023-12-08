72663ade0eb99040ffc63f6d8650fb0d429e501d
Innocent Udo <innocentcharlesudo@gmail.com>
2023-12-08 18:15:02 +0100

    Service Discovery

---
+++ Section8/eurekaserver/src/main/java/dev/Innocent/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
