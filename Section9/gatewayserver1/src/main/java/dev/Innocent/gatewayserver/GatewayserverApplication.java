17c401a644e81d7cf71b0e90aa9a598e1efbf485
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-12 22:04:34 +0100

    API Gateway

--- Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/GatewayserverApplication.java
+++ Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,34 @@ package dev.Innocent.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator innocentBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/udobank/accounts/**")
+						.filters(f -> f.rewritePath("/udobank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+
+				.route(p -> p
+						.path("/udobank/loan/**")
+						.filters(f -> f.rewritePath("/udobank/loan/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOAN"))
+
+				.route(p -> p
+						.path("/udobank/card/**")
+						.filters(f -> f.rewritePath("/udobank/card/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARD")).build();
 	}
 
 }
