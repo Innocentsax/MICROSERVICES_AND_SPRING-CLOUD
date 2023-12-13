2e6068cb7c1332eed7d64f690bdc94dffefc8c1a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-13 13:37:42 +0100

    API Gateway Filters

--- Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/FilterUtility.java
+++ Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/FilterUtility.java
@@ -1,4 +1,30 @@
 package dev.Innocent.gatewayserver.filters;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.List;
+
+
+@Component
 public class FilterUtility {
+    public static final  String CORRELATION_ID = "udobank-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeaders){
+        if(requestHeaders.get(CORRELATION_ID) != null){
+            List<String> requestHeaderList = requestHeaders.get(CORRELATION_ID);
+            return requestHeaderList.stream().findFirst().get();
+        }
+        else{
+            return null;
+        }
+    }
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value){
+        return exchange.mutate().request(exchange.getRequest().mutate().header(name, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId){
+        return this.setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
 }
--- Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/RequestTraceFilter.java
+++ Section9/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/RequestTraceFilter.java
@@ -1,4 +1,22 @@
 package dev.Innocent.gatewayserver.filters;
 
-public class RequestTraceFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(1)
+@Component
+public class RequestTraceFilter implements GlobalFilter {
+    /**
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
 }
