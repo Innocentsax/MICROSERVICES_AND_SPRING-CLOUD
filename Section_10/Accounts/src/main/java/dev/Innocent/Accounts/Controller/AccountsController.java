15b7cb5d3b0938d5f3c9d00cecfa38f6e13c9482
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-16 22:00:35 +0100

    Adding Retry Pattern in Account

--- Section_10/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
+++ Section_10/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
@@ -6,6 +6,7 @@ import dev.Innocent.Accounts.DTO.CustomerDTO;
 import dev.Innocent.Accounts.DTO.Response.ErrorResponseDTO;
 import dev.Innocent.Accounts.DTO.Response.ResponseDTO;
 import dev.Innocent.Accounts.Service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -31,6 +34,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     private final IAccountsService iAccountsService;
 
@@ -183,9 +188,16 @@ public class AccountsController {
                     )
             )
     })
+    @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo(){
+        logger.debug("getBuildInfo() method Invoked");
         return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildFallback(Throwable throwable){
+        logger.debug("getBuildInfoFallback() method Invoked");
+        return ResponseEntity.status(HttpStatus.OK).body("0.9");
     }
 
     @Operation(
--- Section_10/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/GatewayserverApplication.java
+++ Section_10/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,12 @@
 package dev.Innocent.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -45,6 +50,12 @@ public class GatewayserverApplication {
 								.circuitBreaker(config -> config.setName("cardCircuitBreaker")
 										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://CARD")).build();
+	}
+
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).timeLimiterConfig(
+						TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
 	}
 
 }
