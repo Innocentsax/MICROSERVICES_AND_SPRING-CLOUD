1bcce3ec92d7978b5e941777a209da8c551ee236
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-17 11:24:38 +0100

    RateLimiter Pattern in Single App (Account)

--- Section_10/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
+++ Section_10/Accounts/src/main/java/dev/Innocent/Accounts/Controller/AccountsController.java
@@ -6,6 +6,7 @@ import dev.Innocent.Accounts.DTO.CustomerDTO;
 import dev.Innocent.Accounts.DTO.Response.ErrorResponseDTO;
 import dev.Innocent.Accounts.DTO.Response.ResponseDTO;
 import dev.Innocent.Accounts.Service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -217,10 +218,16 @@ public class AccountsController {
                     )
             )
     })
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
