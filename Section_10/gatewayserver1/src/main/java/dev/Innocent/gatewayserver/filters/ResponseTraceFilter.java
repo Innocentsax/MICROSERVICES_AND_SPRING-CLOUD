f441bc4965b8003a5c285973f281a57a34ce0638
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-17 10:17:53 +0100

    Adding Retry Pattern in Account2

--- Section_10/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/ResponseTraceFilter.java
+++ Section_10/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/filters/ResponseTraceFilter.java
@@ -21,8 +21,11 @@ public class ResponseTraceFilter {
             return chain.filter(exchange).then(Mono.fromRunnable(() ->{
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
                 String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+
+                if(!(exchange.getResponse().getHeaders().containsKey(filterUtility.CORRELATION_ID))){
+                    logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                    exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+                }
             }));
         };
     }
