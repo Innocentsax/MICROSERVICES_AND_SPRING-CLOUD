c6d56d9c7671882ee7c18330fb442961c55de403
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-22 14:57:45 +0100

    Microservices Security Config

---
+++ Section_12/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package dev.Innocent.gatewayserver.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity.authorizeExchange(exchange -> exchange.pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("/udobank/accounts/**").authenticated()
+                .pathMatchers("/udobank/card/**").authenticated()
+                .pathMatchers("/udobank/loan/**").authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(Customizer.withDefaults()));
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+        return serverHttpSecurity.build();
+    }
+}
