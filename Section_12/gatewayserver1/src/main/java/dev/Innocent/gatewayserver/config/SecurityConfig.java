267ef88db2d5fa32e9b5e5ca8a71076795e3e3cb
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-22 16:41:52 +0100

    Microservices Security Config and HasRole

--- Section_12/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/config/SecurityConfig.java
+++ Section_12/gatewayserver1/src/main/java/dev/Innocent/gatewayserver/config/SecurityConfig.java
@@ -16,9 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
         serverHttpSecurity.authorizeExchange(exchange -> exchange.pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/udobank/accounts/**").authenticated()
-                .pathMatchers("/udobank/card/**").authenticated()
-                .pathMatchers("/udobank/loan/**").authenticated())
+                .pathMatchers("/udobank/accounts/**").hasRole("ACCOUNTS")
+                .pathMatchers("/udobank/card/**").hasRole("CARD")
+                .pathMatchers("/udobank/loan/**").hasRole("LOAN"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
                         .jwt(Customizer.withDefaults()));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
