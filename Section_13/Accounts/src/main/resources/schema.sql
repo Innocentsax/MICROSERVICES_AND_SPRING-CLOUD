d65a2170da5cc648287eddcb069bf559152370eb
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-24 00:09:20 +0100

    Event Driven microservices update Account ms

--- Section_13/Accounts/src/main/resources/schema.sql
+++ Section_13/Accounts/src/main/resources/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
    `account_number` int AUTO_INCREMENT  PRIMARY KEY,
   `account_type` varchar(100) NOT NULL,
   `branch_address` varchar(200) NOT NULL,
+   `communication_sw` BOOLEAN,
   `created_at` date NOT NULL,
    `created_by` varchar(20) NOT NULL,
    `updated_at` date DEFAULT NULL,
