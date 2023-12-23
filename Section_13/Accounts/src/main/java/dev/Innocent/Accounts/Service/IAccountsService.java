75dfcaf50043f3f9c0923445c1405be0eb43a381
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-24 00:09:09 +0100

    Event Driven microservices update Account ms

--- Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Service/IAccountsService.java
+++ Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Service/IAccountsService.java
@@ -25,4 +25,10 @@ public interface IAccountsService {
      * @return boolean indicating if the delete of Account details is successful or not
      */
     boolean deleteAccount(String mobileNumber);
+
+    /**
+     * @param accountNumber - Long
+     * @return boolean indicating if the update of communication status is successful or not
+     */
+    boolean updateCommunicationStatus(Long accountNumber);
 }
