0b73b70c0b990fc8257991577b1a05dc9e4e9028
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-24 00:07:53 +0100

    Event Driven microservices update Account ms

--- Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Service/ServiceImpl/AccountsServiceImpl.java
+++ Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Service/ServiceImpl/AccountsServiceImpl.java
@@ -128,4 +128,22 @@ public class AccountsServiceImpl implements IAccountsService {
         customerRepository.deleteById(customer.getCustomerId());
         return true;
     }
+
+    /**
+     * @param accountNumber - Long
+     * @return boolean indicating if the update of communication status is successful or not
+     */
+    @Override
+    public boolean updateCommunicationStatus(Long accountNumber) {
+        boolean isUpdated = false;
+        if(accountNumber != null){
+            Accounts accounts = accountsRepository.findById(accountNumber).orElseThrow(
+                    () -> new ResourceNotFoundException("Account", "AccountNumber", accountNumber.toString())
+            );
+            accounts.setCommunicationSw(true);
+            accountsRepository.save(accounts);
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
 }
