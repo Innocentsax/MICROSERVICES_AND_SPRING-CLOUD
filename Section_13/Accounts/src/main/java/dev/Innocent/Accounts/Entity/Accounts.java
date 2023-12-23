de3611bba8318bc03b686504b618d32977f1d0c1
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-24 00:07:24 +0100

    Event Driven microservices update Account ms

--- Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Entity/Accounts.java
+++ Section_13/Accounts/src/main/java/dev/Innocent/Accounts/Entity/Accounts.java
@@ -25,4 +25,7 @@ public class Accounts extends BaseEntity{
 
     @Column(name = "branch_address")
     private String branchAddress;
+
+    @Column(name = "communication_sw")
+    private Boolean communicationSw;
 }
